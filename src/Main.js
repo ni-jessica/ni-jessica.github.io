cd377aa38b62d8acc0bf001992089792c33f07d7
ni-jessica <jessica_ni@brown.edu>
2023-05-19 05:38:34 -0700

    add contact

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import "./Main.css";
 import { TypingStep } from "typing-effect-reactjs";
 import MeetkaiGif from "./meetkai.gif";
 import HackAtBrown from "./hab.png";
+import { SocialIcon } from "react-social-icons";
 
 const TypeHeading = () => {
   return (
@@ -37,7 +38,7 @@ const Nav = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-10">
       <div id="show">
-        <ul className="flex text-center md:text-right justify-center md:justify-end items-center bg-[#080357] p-6 shadow-xl opacity-90 text-sm sm:text-base">
+        <ul className="flex text-center md:text-right justify-center md:justify-end items-center bg-[#080357] p-6 shadow-xl text-sm sm:text-base">
           <li className="mr-6">
             <a
               className="text-[#F9F7F1] hover:text-[#848089] hover:underline hover:underline-offset-2"
@@ -76,9 +77,7 @@ const Nav = () => {
   );
 };
 
-
 function Main() {
-
   // prevents scrolling on first 10 seconds of load
 
   // document.body.setAttribute("style", "overflow:hidden;");
@@ -101,7 +100,7 @@ function Main() {
             id="about"
             className="text-center md:text-left space-y-2 text-base sm:text-lg pb-24"
           >
-            <h1 className="text-2xl md:text-5xl mt-24 text-[#848089] ">
+            <h1 className="text-2xl md:text-5xl mt-24 text-[#0C058A] ">
               About Me
             </h1>
             <p className="md:pt-4">
@@ -123,20 +122,20 @@ function Main() {
 
       <div id="show">
         <div id="background" className="text-base md:text-lg md:mt-4 mb-32">
-          <h1 className="text-center md:text-left text-2xl md:text-5xl text-[#848089] ">
+          <h1 className="text-center md:text-left text-2xl md:text-5xl text-[#0C058A] ">
             Background
           </h1>
           <div id="work experience" className="lg:max-h-full pb-8">
             <div className="md:pt-2 grid grid-flow-row auto-cols-[minmax(0,_1fr)] lg:grid-flow-col lg:gap-x-12">
               <div>
-                <h2 className="text-xl md:text-3xl pt-6 text-[#79025B] ">
+                <h2 className="text-xl md:text-3xl pt-6 text-[#7A0050] ">
                   Work Experience
                 </h2>
 
-                <p className="pt-4 font-semibold text-base md:text-xl text-[#79025B]">
+                <p className="pt-4 font-semibold text-base md:text-xl text-[#7A0050]">
                   Meetkai: Software Engineer Intern
                 </p>
-                <p className="text-sm md:text-base text-[#79025B] -mt-1">
+                <p className="text-sm md:text-base text-[#7A0050] -mt-1">
                   Languages & Tools used: Javascript, React, Node.js, HTML, CSS,
                   TailwindCSS, AWS, Prisma Client, Jest, Docker
                 </p>
@@ -214,10 +213,10 @@ function Main() {
                 </a>
               </div>
               <div>
-                <h2 className="text-xl md:text-3xl pt-6 text-[#79025B] ">
+                <h2 className="text-xl md:text-3xl pt-6 text-[#7A0050] ">
                   Activities
                 </h2>
-                <p className="font-semibold text-base md:text-xl pt-4 text-[#79025B]">
+                <p className="font-semibold text-base md:text-xl pt-4 text-[#7A0050]">
                   Hack at Brown
                 </p>
                 <div className="pt-2">
@@ -250,6 +249,8 @@ function Main() {
                       Check out 2023's iteration{" "}
                       <a
                         href="https://2023.hackatbrown.org/"
+                        target="_blank"
+                        rel="noreferrer noopener"
                         className="underline"
                       >
                         here!
@@ -263,32 +264,97 @@ function Main() {
         </div>
 
         <div id="projects">
-          <h1 className="text-2xl md:text-5xl text-center md:text-left text-[#848089] mt-12">
+          <h1 className="text-2xl md:text-5xl text-center md:text-left text-[#848089]- text-[#0C058A] mt-12">
             Projects
           </h1>
-          <div className="pt-8 grid grid-flow-row md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <div className="box bg-blue-100 rounded-lg px-6 py-8">
-              Caching I/O
+          <div className="pt-8 grid grid-flow-row md:grid-cols-2 lg:grid-cols-3 gap-8  text-[#31493b] text-center md:text-left">
+            <div className="box bg-[#DCD8D0] rounded-lg px-6 py-8 shadow-lg hover:shadow-xl hover:scale-[1.03] transition duration-300">
+              <p className="text-lg md:text-2xl font-semibold">Caching I/O</p>
+              <p className="my-6 text-base md:text-xl">
+                Implemented single slot cache to reduce system calls and
+                increase efficiency for buffered read and write.
+              </p>
+              <p>Languages: C</p>
             </div>
-            <div className="box bg-blue-100 rounded-lg px-6 py-8">
-              Weensy OS
+            <div className="box bg-[#DCD8D0] rounded-lg px-6 py-8 shadow-lg hover:shadow-xl hover:scale-[1.03] transition duration-300">
+              <p className="text-lg md:text-2xl font-semibold">Weensy OS</p>
+              <p className="my-6 text-base md:text-xl">
+                Implemented single slot cache to reduce system calls and
+                increase efficiency for buffered read and write.
+              </p>
+              <p>Languages: C</p>
             </div>
-            <div className="box bg-blue-100 rounded-lg px-6 py-8">
-              Dorms at Brown
+            <div className="box bg-[#DCD8D0] rounded-lg px-6 py-8 shadow-lg hover:shadow-xl hover:scale-[1.03] transition duration-300">
+              <p className="text-lg md:text-2xl font-semibold">
+                Dorms at Brown
+              </p>
+              <p className="my-6 text-base md:text-xl">
+                Implemented single slot cache to reduce system calls and
+                increase efficiency for buffered read and write.
+              </p>
+              <p>Languages: C</p>
             </div>
-            <div className="box bg-blue-100 rounded-lg px-6 py-8">Maps</div>
-            <div className="box bg-blue-100 rounded-lg px-6 py-8">Search</div>
-            <div className="box bg-blue-100 rounded-lg px-6 py-8">
-              Personal Website
+            <div className="box bg-[#DCD8D0] rounded-lg px-6 py-8 shadow-lg hover:shadow-xl hover:scale-[1.03] transition duration-300">
+              <p className="text-lg md:text-2xl font-semibold">Maps</p>
+              <p className="my-6 text-base md:text-xl">
+                Implemented single slot cache to reduce system calls and
+                increase efficiency for buffered read and write.
+              </p>
+              <p>Languages: C</p>
+            </div>
+            <div className="box bg-[#DCD8D0] rounded-lg px-6 py-8 shadow-lg hover:shadow-xl hover:scale-[1.03] transition duration-300">
+              <p className="text-lg md:text-2xl font-semibold">Search</p>
+              <p className="my-6 text-base md:text-xl">
+                Implemented single slot cache to reduce system calls and
+                increase efficiency for buffered read and write.
+              </p>
+              <p>Languages: C</p>
+            </div>
+            <div className="box bg-[#DCD8D0] rounded-lg px-6 py-8 shadow-lg hover:shadow-xl hover:scale-[1.03] transition duration-300">
+              <p className="text-lg md:text-2xl font-semibold">
+                Personal Website
+              </p>
+              <p className="my-6 text-base md:text-xl">
+                What you're on right now!
+              </p>
+              <p>Languages: Javascript, React, HTML, CSS</p>
             </div>
           </div>
         </div>
 
         <div id="contact" className="mt-24 text-center">
-          <h1 className="text-2xl md:text-5xl text-center md:text-left text-[#848089] mt-12">
+          <h1 className="text-2xl md:text-5xl text-center text-[#0C058A] mt-12">
             Contact Info
           </h1>
-          Let's connect!
+          <div className="mt-6">
+            <p className="text-lg md:text-2xl">Let's connect {"\u{1F60A}"}</p>
+            <ul className="mt-6 space-x-12 flex flex-row justify-center text-base md:text-lg">
+              <li className="">
+                <SocialIcon
+                  target="_blank"
+                  url="https://www.linkedin.com/in/jessica-ni-45b8001a9/"
+                  bgColor="#0A0476"
+                  className="hover:scale-105 transition duration-300"
+                />
+              </li>
+              <li className="">
+                <SocialIcon
+                  target="_blank"
+                  url="https://github.com/ni-jessica"
+                  bgColor="#0A0476"
+                  className="hover:scale-105 transition duration-300"
+                />
+              </li>
+              <li className="hover:underline underline-offset-2">
+                <SocialIcon
+                  target="_blank"
+                  url="mailto:jessica_ni@brown.edu"
+                  bgColor="#0A0476"
+                  className="hover:scale-105 transition duration-300"
+                />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
